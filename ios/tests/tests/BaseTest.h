e33e92f5f08e6c1061fbf0f26f2346d4b9545bb6
JM <jose.navarro@liferay.com>
2014-03-24 21:45:56 +0100

    Updated copyright

--- ios/tests/tests/BaseTest.h
+++ ios/tests/tests/BaseTest.h
@@ -1,10 +1,16 @@
-//
-//  testsTests.m
-//  testsTests
-//
-//  Created by jmWork on 24/03/14.
-//  Copyright (c) 2014 Liferay. All rights reserved.
-//
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
 #import <XCTest/XCTest.h>
 
@@ -13,6 +19,6 @@
 @interface BaseTest : XCTestCase
 
 @property (strong) NSDictionary* settings;
-@property (strong, readonly) LRSession* session;
+@property (strong) LRSession* session;
 
 @end
--- ios/tests/tests/BaseTest.m
+++ ios/tests/tests/BaseTest.m
@@ -1,10 +1,16 @@
-//
-//  testsTests.m
-//  testsTests
-//
-//  Created by JM on 24/03/14.
-//  Copyright (c) 2014 Liferay. All rights reserved.
-//
+/**
+ * Copyright (c) 2000-2013 Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
 #import "BaseTest.h"
 #import "LRSession.h"
@@ -18,9 +24,11 @@
     
     [self loadSettings];
     
-    _session = [[LRSession alloc] init:[NSString stringWithFormat:@"http://%@", self.settings[@"server"]]
-                              username:self.settings[@"login"]
-                              password:self.settings[@"password"]];
+    NSString *url = [NSString stringWithFormat:@"http://%@", self.settings[@"server"]];
+    
+    self.session = [[LRSession alloc] init:url
+                                  username:self.settings[@"login"]
+                                  password:self.settings[@"password"]];
 }
 
 - (void)tearDown
